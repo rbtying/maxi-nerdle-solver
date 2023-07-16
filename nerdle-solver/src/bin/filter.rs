6a46bd9a31126d77812c211797fd58cd535a412a
Robert Ying <me@robertying.com>
2023-07-16 16:12:30 -0700

    solve a bit smarter

--- nerdle-solver/src/bin/filter.rs
+++ nerdle-solver/src/bin/filter.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+use rand::{seq::SliceRandom, thread_rng};
+
 use nerdle_solver::mask;
 
 fn prompt(s: &'static str) -> String {
@@ -12,31 +14,60 @@ fn prompt(s: &'static str) -> String {
 
 fn main() {
     let args = std::env::args().collect::<Vec<_>>();
-    println!("Reading options from {}", args[1]);
+    print!("Reading options from {}...", args[1]);
     let f = BufReader::new(File::open(&args[1]).unwrap());
     let mut options: Vec<String> = f.lines().map(|s| s.unwrap().trim().to_string()).collect();
-
-    println!("Loaded {} options", options.len());
+    println!("done");
 
     loop {
+        println!("{} options remaining", options.len());
         println!();
-        let guess = prompt("Enter your guess (you can use s for ² and c for ³)");
-        let mask_txt =
-            prompt("Enter your mask (G or 2 for green; P or 1 for purple; B or 0 for black)");
+
+        println!("Computing best next guess...");
+        let best_guess = if options.len() < 1_000 {
+            let best_guess = mask::compute_best_guess(&options);
+            println!("{}, score: {}", best_guess.0, best_guess.1);
+            best_guess.0.to_string()
+        } else {
+            let subset: Vec<_> = options.choose_multiple(&mut thread_rng(), 500).collect();
+            let best_guess = mask::compute_best_guess(&subset);
+            println!(
+                "{}, score: {} (based on 500 randomly-selected examples)",
+                best_guess.0, best_guess.1
+            );
+            best_guess.0.to_string()
+        };
+        println!();
+
+        for v in options.choose_multiple(&mut thread_rng(), 25).take(25) {
+            println!("- {}", v);
+        }
+        if options.len() > 25 {
+            println!("- ...");
+        }
+
+        if options.len() == 1 {
+            return;
+        }
+
+        println!();
+        let mut guess = prompt("Enter your guess (you can use s for ² and c for ³)");
+
+        if guess.trim().is_empty() {
+            println!("Using {} as the guess", best_guess);
+            guess = best_guess;
+        }
+
+        let mask_txt = loop {
+            let txt =
+                prompt("Enter your mask (G or 2 for green; P or 1 for purple; B or 0 for black)");
+            if !txt.is_empty() {
+                break txt;
+            }
+        };
+
         if let Some(m) = mask::parse_mask_results(&guess, &mask_txt) {
             options.retain(|o| mask::matches_mask(o, &m));
-            println!("{} options remaining", options.len());
-            println!();
-            for v in options.iter().take(25) {
-                println!("- {}", v);
-            }
-            if options.len() > 25 {
-                println!("- ...");
-            }
-
-            if options.len() == 1 {
-                return;
-            }
         }
     }
 }
--- nerdle-solver/src/mask.rs
+++ nerdle-solver/src/mask.rs
@@ -1,10 +1,12 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{BTreeSet, HashMap};
 
-#[derive(Default, Debug)]
+use rayon::prelude::*;
+
+#[derive(Clone, Default, Debug, PartialEq, Eq, Hash)]
 pub struct Masks {
-    correct: HashSet<(usize, char)>,
-    incorrect: HashSet<(usize, char)>,
-    not_present: HashSet<(usize, char)>,
+    correct: BTreeSet<(usize, char)>,
+    incorrect: BTreeSet<(usize, char)>,
+    not_present: BTreeSet<(usize, char)>,
 }
 
 // Parse the provided string to an evaluation mask
@@ -58,7 +60,7 @@ pub fn matches_mask(guess: &str, mask: &Masks) -> bool {
     let chars = guess
         .chars()
         .enumerate()
-        .collect::<HashSet<(usize, char)>>();
+        .collect::<BTreeSet<(usize, char)>>();
 
     if mask.not_present.intersection(&chars).next().is_some() {
         return false;
@@ -93,4 +95,96 @@ pub fn matches_mask(guess: &str, mask: &Masks) -> bool {
     }
 
     true
+}
+
+pub fn score(guess: &str, truth: &str) -> Masks {
+    let mut m = Masks::default();
+
+    let mut truth_char_counts: HashMap<char, usize> = HashMap::new();
+    for (idx, (g, t)) in guess.chars().zip(truth.chars()).enumerate() {
+        if g == t {
+            m.correct.insert((idx, g));
+        } else {
+            *truth_char_counts.entry(t).or_default() += 1;
+        }
+    }
+
+    for (idx, g) in guess.chars().enumerate() {
+        if m.correct.contains(&(idx, g)) {
+            continue;
+        }
+        let v = truth_char_counts.entry(g).or_default();
+        if *v > 0 {
+            *v -= 1;
+            m.incorrect.insert((idx, g));
+        } else {
+            m.not_present.insert((idx, g));
+        }
+    }
+
+    m
+}
+
+/// Compute the guess which has the highest entropy in the corpus
+pub fn compute_best_guess<T: AsRef<str> + Sync>(corpus: &[T]) -> (&str, f64) {
+    corpus
+        .par_iter()
+        .map(|w| (w.as_ref(), compute_entropy(w.as_ref(), corpus)))
+        .max_by(|a, b| a.1.total_cmp(&b.1))
+        .unwrap()
+}
+
+// Compute the entropy of the given guess `x` against the corpus.
+//
+// Note that we don't subtract out the corpus.len().log2(), since that doesn't
+// change the ordering of the entropies.
+pub fn compute_entropy<T: AsRef<str>>(x: &str, corpus: &[T]) -> f64 {
+    let mut masks: HashMap<Masks, usize> = HashMap::new();
+    for v in corpus {
+        let m = score(x, v.as_ref());
+        *masks.entry(m).or_default() += 1;
+    }
+
+    let mut t = 0.;
+
+    for (m, ct) in masks {
+        let matching = corpus
+            .iter()
+            .filter(|c| matches_mask(c.as_ref(), &m))
+            .count();
+        let p = -(matching as f64).log2();
+
+        t += ct as f64 * p;
+    }
+    t
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{compute_best_guess, compute_entropy, score, Masks};
+
+    #[test]
+    fn test_score() {
+        let m = score("abc", "cbd");
+        assert_eq!(
+            m,
+            Masks {
+                correct: [(1, 'b')].into_iter().collect(),
+                incorrect: [(2, 'c')].into_iter().collect(),
+                not_present: [(0, 'a')].into_iter().collect(),
+            }
+        )
+    }
+
+    #[test]
+    fn test_entropy() {
+        let e = compute_entropy("abc", &["abc", "abd", "aba"]);
+        assert_eq!(e, -2.)
+    }
+
+    #[test]
+    fn test_compute_best_guess() {
+        let g = compute_best_guess(&["abc", "abd", "aba"]);
+        assert_eq!(g, ("aba", -2.));
+    }
 }
